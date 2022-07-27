9f04b4f1dfe5113034700c1db51a8fcb1ee48aa1
haoyuchou <haoyuchou123@gmail.com>
2022-07-27 17:14:21 +0800

    fix environment variable

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
-    plugins: [
-      "tailwindcss-react-native/babel",
-      "module:react-native-dotenv",
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
-    ],
+    presets: ["babel-preset-expo"],
+    plugins: ["tailwindcss-react-native/babel", "module:react-native-dotenv"],
   };
 };
